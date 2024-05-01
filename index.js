ef5f147e2928b88e9658db6594bcfd47dff84aff
Syed Shahan Hussain <shahanhussain9105@gmail.com>
2024-05-02 02:54:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')(process.env.PORT || 3001, {
   cors: {
-    origin: 'https://easyconnect-one.vercel.app/',
+    origin: 'https://easyconnect-one.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
